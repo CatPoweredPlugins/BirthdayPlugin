8d83cf84862e93ddd14d5fed63ccfdd2a648c920
Rudokhvist <kotlyar.andrey@gmail.com>
2023-11-22 21:57:16 +0200

    Update BirthdayPlugin.cs

--- BirthdayPlugin/BirthdayPlugin.cs
+++ BirthdayPlugin/BirthdayPlugin.cs
@@ -12,7 +12,7 @@ using Newtonsoft.Json.Linq;
 namespace BirthdayPlugin {
 #pragma warning disable CA1812 // ASF uses this class during runtime
 	[UsedImplicitly]
-	internal sealed class BirthdayPlugin : IBotModules, IDisposable {
+	internal class BirthdayPlugin : IBotModules, IDisposable {
 		internal class Birthday {
 			public DateTime Date;
 			public string? Name;
